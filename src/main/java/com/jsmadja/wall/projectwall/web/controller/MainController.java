d1f4f104f289401e62ed12a7418ae324218e0a6d
jsmadja <jsmadja@xebia.fr>
2011-02-20 15:10:30 +0100

    cleaning

--- src/main/java/com/jsmadja/wall/projectwall/web/controller/MainController.java
+++ src/main/java/com/jsmadja/wall/projectwall/web/controller/MainController.java
@@ -4,23 +4,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
-
-import com.jsmadja.wall.projectwall.HudsonService;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
-	
-	@GET
-	@RequestMapping
-	protected ModelAndView handleRequestInternal(HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
-		return new ModelAndView("index", "from", "from  spring MVC !");
-	}
+
+    @GET
+    @RequestMapping
+    protected ModelAndView handleRequestInternal(HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        return new ModelAndView("index", "from", "from  spring MVC !");
+    }
 
 }
