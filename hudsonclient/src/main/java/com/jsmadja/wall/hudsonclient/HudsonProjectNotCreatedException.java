c781a9d3205556cd463ea41c71be999dd4a8b90d
jsmadja <julien.smadja@gmail.com>
2011-02-24 20:49:44 +0100

    move hudson files to hudson client project

---
+++ hudsonclient/src/main/java/com/jsmadja/wall/hudsonclient/HudsonProjectNotCreatedException.java
@@ -0,0 +1,12 @@
+package com.jsmadja.wall.hudsonclient;
+
+
+public final class HudsonProjectNotCreatedException extends Exception {
+
+    private static final long serialVersionUID = -6952748181282352092L;
+
+    public HudsonProjectNotCreatedException(Throwable cause) {
+        super(cause);
+    }
+
+}
