359ca75ce26660a51e6a8e4241fd82b96336a811
Arnaud LEMAIRE <alemaire@norad.fr>
2011-02-24 15:04:14 +0100

    - create server and hudson client project - add fail project

---
+++ failProject/src/test/java/net/awired/jwall/FailTest.java
@@ -0,0 +1,12 @@
+package net.awired.jwall;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FailTest {
+
+	@Test
+	public void should_fail() {
+		Assert.fail("this is a fail project");
+	}
+}
