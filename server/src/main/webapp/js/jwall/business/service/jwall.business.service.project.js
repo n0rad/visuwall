81ea9ac3ae694ebbf41434d13bfdc9dcaed04729
Arnaud LEMAIRE <alemaire@norad.fr>
2011-02-25 13:26:11 +0100

    - change getStatus to call findAllProject for the moment

--- server/src/main/webapp/js/jwall/business/service/jwall.business.service.project.js
+++ server/src/main/webapp/js/jwall/business/service/jwall.business.service.project.js
@@ -1,9 +1,23 @@
 jwall.business.service.project = {
+
+	projects : {},
+		
 	serviceUri : "project",
+	
 		
 	getProjects : function(callback) {
 		LOG.debug('calling getProjets');
-		$.getJSON(this.serviceUri, {}, callback);
+		
+		$this = this;
+		$.getJSON(this.serviceUri, {}, function(data) {
+			
+			for (var i = 0; i < data.length; i++) {
+				var currentProject = data[i];
+				$this.projects[currentProject.name] = currentProject;
+			}
+			
+			callback(data);
+		});
 	},
 	
 	getProject : function(projectName, callback) {
@@ -11,8 +25,30 @@ jwall.business.service.project = {
 	},
 	
 	update : function() {
+		$this = this;
 		$.getJSON("project/status", {}, function(data) {
 			LOG.debug("receive update");
+			
+			for (var i = 0; i < data.length; i++) {
+				var currentProject = data[i];
+				var myBuildingStatus = $this.projects[currentProject.name].hudsonProject.building;
+				var myLastBuildNum = $this.projects[currentProject.name].hudsonProject.lastBuildNumber;
+				var serverBuilderStatus = currentProject.building;
+				var serverLastBuildNum = currentProject.lastBuildId;
+				
+				if (myBuildingStatus == true && serverBuilderStatus == false) {
+					LOG.info("build is over on server for " + currentProject.name);
+				}
+				if (myBuildingStatus == false && serverBuilderStatus == true) {
+					LOG.info("server is now building" + currentProject.name);
+				}
+				
+				if (myLastBuildNum != serverLastBuildNum) {
+					LOG.info("server build is updated");
+				}
+				
+				
+			}
 		});
 	}
 
