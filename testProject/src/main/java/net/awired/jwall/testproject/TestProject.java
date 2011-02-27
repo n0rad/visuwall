23765d9421e10a255e96874fc00b535173ee8cc4
Arnaud LEMAIRE <alemaire@norad.fr>
2011-02-27 19:05:24 +0100

    - update test project to run as success with parameter

--- testProject/src/main/java/net/awired/jwall/testproject/TestProject.java
+++ testProject/src/main/java/net/awired/jwall/testproject/TestProject.java
@@ -8,7 +8,7 @@ public class TestProject {
 		if (value != null) {
 			if ("fail".equals(value)) {
 				Assert.fail("this is a fail project");
-			} else {
+			} else if ("error".equals(value)) {
 				throw new RuntimeException("This is a Error project");
 			}
 		}
