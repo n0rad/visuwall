d6b8589bc0cdb80f11721df35348eeddf1f8e1ab
jsmadja <julien.smadja@gmail.com>
2011-03-07 16:29:13 +0100

    add findBuildByProjectNameAndBuilderNumber

---
+++ server/src/main/java/com/jsmadja/wall/projectwall/service/BuildNotFoundException.java
@@ -0,0 +1,12 @@
+package com.jsmadja.wall.projectwall.service;
+
+public class BuildNotFoundException extends Exception {
+
+    private static final long serialVersionUID = 615519485544622495L;
+
+    public BuildNotFoundException(String cause) {
+        super(cause);
+    }
+
+
+}
