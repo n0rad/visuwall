bfc0fc79d226f8fe58874563fc81a25244ad306f
Arnaud LEMAIRE <alemaire@norad.fr>
2011-03-11 16:31:38 +0100

    - add projects for integration

---
+++ testProjects/successproject/src/main/java/net/awired/jwall/testproject/TestProject.java
@@ -0,0 +1,17 @@
+package net.awired.jwall.testproject;
+
+import org.junit.Assert;
+
+public class TestProject {
+	public static void main(String[] args) {
+		String value = System.getenv("status");
+		if (value != null) {
+			System.out.println("status value :" + value);
+			if ("fail".equals(value)) {
+				Assert.fail("this is a fail project");
+			} else if ("error".equals(value)) {
+				throw new RuntimeException("This is a Error project");
+			}
+		}
+	}
+}
