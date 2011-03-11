2103775f9890439564fa55dbe3921a4a1689c135
jsmadja <julien.smadja@gmail.com>
2011-03-11 16:18:57 +0100

    change hudson target in tests

--- hudsonclient/src/test/java/com/jsmadja/wall/hudsonclient/it/HudsonTest.java
+++ hudsonclient/src/test/java/com/jsmadja/wall/hudsonclient/it/HudsonTest.java
@@ -8,20 +8,18 @@ import com.jsmadja.wall.hudsonclient.HudsonProjectNotFoundException;
 
 public class HudsonTest {
 
-    private static final String FLUXX_HUDSON_URL = "http://fluxx.fr.cr:8080/hudson";
-    private static final String AWIRED_HUDSON_URL = "http://ci.awired.net/jenkins";
+    private static final String HUDSON_URL = "http://ci.jwall.awired.net";
 
-    private Hudson hudsonFluxx = new Hudson(FLUXX_HUDSON_URL);
-    private Hudson hudsonAwired = new Hudson(AWIRED_HUDSON_URL);
+    private Hudson hudson = new Hudson(HUDSON_URL);
 
     @Test(expected = HudsonBuildNotFoundException.class)
     public void should_throw_an_exception_when_searching_an_inexistant_build() throws HudsonBuildNotFoundException {
-        hudsonFluxx.findBuild("dev-radar", -1);
+        hudson.findBuild("dev-radar", -1);
     }
 
     @Test
     public void should_find_not_built_project() throws HudsonProjectNotFoundException {
-        hudsonAwired.findProject("ajsl");
+        hudson.findProject("neverbuild");
     }
 
 }
