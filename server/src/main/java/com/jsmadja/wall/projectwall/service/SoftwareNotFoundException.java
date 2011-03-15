252e35569bd52f2e5ac0ddae3b082d15af7fa3ef
jsmadja <julien.smadja@gmail.com>
2011-03-15 18:33:01 +0100

    add persistence capabilities

---
+++ server/src/main/java/com/jsmadja/wall/projectwall/service/SoftwareNotFoundException.java
@@ -0,0 +1,11 @@
+package com.jsmadja.wall.projectwall.service;
+
+public class SoftwareNotFoundException extends Exception {
+
+    private static final long serialVersionUID = 3207668047126605653L;
+
+    public SoftwareNotFoundException(String cause) {
+        super(cause);
+    }
+
+}
