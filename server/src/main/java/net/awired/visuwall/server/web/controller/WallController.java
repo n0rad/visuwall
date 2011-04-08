0259cf208b636a368c4323f7150d25bd4e7182ad
jsmadja <julien.smadja@gmail.com>
2011-04-08 17:00:14 +0200

    add config

--- server/src/main/java/net/awired/visuwall/server/web/controller/WallController.java
+++ server/src/main/java/net/awired/visuwall/server/web/controller/WallController.java
@@ -6,8 +6,6 @@ import java.util.Set;
 import javax.annotation.PostConstruct;
 
 import net.awired.visuwall.api.domain.ProjectStatus;
-import net.awired.visuwall.server.domain.Software;
-import net.awired.visuwall.server.domain.SoftwareAccess;
 import net.awired.visuwall.server.domain.Wall;
 import net.awired.visuwall.server.service.WallService;
 
@@ -25,74 +23,72 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/wall")
 public class WallController {
 
-	private static final Logger LOG = LoggerFactory
-			.getLogger(WallController.class.getName());
-	private static final String WALL_JSP = "wall/wall";
+    private static final Logger LOG = LoggerFactory
+    .getLogger(WallController.class.getName());
+    private static final String WALL_JSP = "wall/wall";
 
-	@Autowired
-	private WallService wallService;
+    @Autowired
+    private WallService wallService;
 
-	Wall wall;
+    Wall wall;
 
-	public WallController() {
-		wall = new Wall("orange-vallee");
-//		 wall.addSoftwareAccess(new SoftwareAccess(Software.HUDSON,
-//		 "http://integration.wormee.orange-vallee.net:8080/hudson"));
-//		 wall.addSoftwareAccess(new SoftwareAccess(Software.SONAR,
-//		 "http://integration.wormee.orange-vallee.net:9000"));
-//		wall.addSoftwareAccess(new SoftwareAccess(Software.HUDSON,
-//				"http://ci.awired.net/jenkins"));
-		// wall.addSoftwareAccess(new SoftwareAccess(Software.HUDSON,
-		// "http://ci.visuwall.awired.net"));
-//		wall.addSoftwareAccess(new SoftwareAccess(Software.SONAR,
-//				"http://sonar.awired.net"));
-		 wall.addSoftwareAccess(new SoftwareAccess(Software.HUDSON,
-		 "http://fluxx.fr.cr:8080/hudson"));
-		 wall.addSoftwareAccess(new SoftwareAccess(Software.SONAR,
-		 "http://fluxx.fr.cr:9000"));
-		wall.discoverProjects();
-	}
+    public WallController() {
+        wall = new Wall("orange-vallee");
+        //        wall.addSoftwareAccess(new SoftwareAccess(Software.HUDSON, "http://10.2.40.60/lifeisbetteron/jenkins"));
+        //        wall.addSoftwareAccess(new SoftwareAccess(Software.SONAR, "http://10.2.40.60/lifeisbetteron/sonar"));
+        //		wall.addSoftwareAccess(new SoftwareAccess(Software.HUDSON,
+        //				"http://ci.awired.net/jenkins"));
+        // wall.addSoftwareAccess(new SoftwareAccess(Software.HUDSON,
+        // "http://ci.visuwall.awired.net"));
+        //		wall.addSoftwareAccess(new SoftwareAccess(Software.SONAR,
+        //				"http://sonar.awired.net"));
+        //		 wall.addSoftwareAccess(new SoftwareAccess(Software.HUDSON,
+        //		 "http://fluxx.fr.cr:8080/hudson"));
+        //		 wall.addSoftwareAccess(new SoftwareAccess(Software.SONAR,
+        //		 "http://fluxx.fr.cr:9000"));
+        wall.discoverProjects();
+    }
 
-	@PostConstruct
-	public void postConstruct() {
-		wallService.addWall(wall);
-	}
+    @PostConstruct
+    public void postConstruct() {
+        wallService.addWall(wall);
+    }
 
-	// //////////////////////////////////////////////////////////////////
+    // //////////////////////////////////////////////////////////////////
 
-	@RequestMapping
-	public @ResponseBody
-	Set<String> getWallNames() {
-		return wallService.getWallNames();
-	}
+    @RequestMapping
+    public @ResponseBody
+    Set<String> getWallNames() {
+        return wallService.getWallNames();
+    }
 
-	@RequestMapping("{wallName}")
-	public @ResponseBody
-	Wall getProjects(@PathVariable String wallName) {
-		Wall wall = wallService.getWall(wallName);
-		LOG.info("Projects collection size :" + wall.getProjects().size());
-		return wall;
-	}
+    @RequestMapping("{wallName}")
+    public @ResponseBody
+    Wall getProjects(@PathVariable String wallName) {
+        Wall wall = wallService.getWall(wallName);
+        LOG.info("Projects collection size :" + wall.getProjects().size());
+        return wall;
+    }
 
-	@RequestMapping("{wallName}/status")
-	public @ResponseBody
-	List<ProjectStatus> getStatus(@PathVariable String wallName) {
-		return wallService.getStatus(wallName);
-	}
+    @RequestMapping("{wallName}/status")
+    public @ResponseBody
+    List<ProjectStatus> getStatus(@PathVariable String wallName) {
+        return wallService.getStatus(wallName);
+    }
 
-	@RequestMapping(value = "create", method = RequestMethod.GET)
-	public String getCreate() {
-		return WALL_JSP;
-	}
+    @RequestMapping(value = "create", method = RequestMethod.GET)
+    public String getCreate() {
+        return WALL_JSP;
+    }
 
-	@RequestMapping(value = "create", method = RequestMethod.POST)
-	public ModelAndView create() {
-		return null;
-	}
+    @RequestMapping(value = "create", method = RequestMethod.POST)
+    public ModelAndView create() {
+        return null;
+    }
 
-	@RequestMapping(value = "{wallName}", method = RequestMethod.DELETE)
-	public void DeleteWall(@PathVariable String wallName) {
+    @RequestMapping(value = "{wallName}", method = RequestMethod.DELETE)
+    public void DeleteWall(@PathVariable String wallName) {
 
-	}
+    }
 
 }
