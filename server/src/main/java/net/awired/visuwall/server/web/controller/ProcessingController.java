5957bb409b0d104eb97e7e514cb4f010388fde9f
Arnaud LEMAIRE <alemaire@norad.fr>
2011-04-17 22:44:39 +0200

    - put back getfinishtime controller method

--- server/src/main/java/net/awired/visuwall/server/web/controller/ProcessingController.java
+++ server/src/main/java/net/awired/visuwall/server/web/controller/ProcessingController.java
@@ -1,21 +1,32 @@
 package net.awired.visuwall.server.web.controller;
 
+import java.util.Date;
+
+import net.awired.visuwall.server.domain.Wall;
+import net.awired.visuwall.server.service.ProjectService;
 import net.awired.visuwall.server.service.WallService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/processing")
 public class ProcessingController {
 
-    @Autowired
-    private WallService wallService;
+	@Autowired
+	private WallService wallService;
 
-    //	@RequestMapping("finishTime")
-    //	public @ResponseBody Date getFinishTime(@RequestParam String wallName,
-    //			@RequestParam String projectName) throws Exception {
-    //		return wallService.getWall(wallName).getEstimatedFinishTime(projectName);
-    //	}
+	@Autowired
+	private ProjectService projectService;
+
+	@RequestMapping("finishTime")
+	public @ResponseBody
+	Date getFinishTime(@RequestParam String wallName,
+			@RequestParam String projectName) throws Exception {
+		Wall wall = wallService.find(wallName);
+		return projectService.getEstimatedFinishTime(wall, projectName);
+	}
 }
