25081dd2eea608d1258964dc1a4ffd21398b9798
jsmadja <julien.smadja@gmail.com>
2011-05-04 11:10:28 +0200

    test cleaning

--- core/src/main/java/net/awired/visuwall/core/service/ProjectService.java
+++ core/src/main/java/net/awired/visuwall/core/service/ProjectService.java
@@ -42,154 +42,155 @@ import com.google.common.base.Preconditions;
 @Service
 public class ProjectService {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ProjectService.class);
+	private static final Logger LOG = LoggerFactory.getLogger(ProjectService.class);
 
-    @Autowired
-    ProjectMergeService projectMergeService;
+	@Autowired
+	ProjectMergeService projectMergeService;
 
-    private static final int PROJECT_NOT_BUILT_ID = -1;
+	private static final int PROJECT_NOT_BUILT_ID = -1;
 
-    @Transient
-    String[] metrics = new String[]{"coverage", "ncloc", "violations_density"};
+	@Transient
+	String[] metrics = new String[] { "coverage", "ncloc", "violations_density", "it_coverage" };
 
-    public void updateWallProjects(Wall wall) {
-        Preconditions.checkNotNull(wall, "wall is a mandatory parameter");
+	public void updateWallProjects(Wall wall) {
+		Preconditions.checkNotNull(wall, "wall is a mandatory parameter");
 
-        for(BuildConnectionPlugin buildService : wall.getPluginHolder().getBuildServices()) {
-            List<ProjectId> discoveredProjects = buildService.findAllProjects();
-            for(ProjectId discoveredProjectId : discoveredProjects) {
+		for (BuildConnectionPlugin buildService : wall.getPluginHolder().getBuildServices()) {
+			List<ProjectId> discoveredProjects = buildService.findAllProjects();
+			for (ProjectId discoveredProjectId : discoveredProjects) {
 
-                Project project;
-                try {
-                    project = wall.getProjectByProjectId(discoveredProjectId);
-                } catch (ProjectNotFoundException e) {
-                    project = new Project(discoveredProjectId);
-                    wall.getProjects().add(project);
-                }
+				Project project;
+				try {
+					project = wall.getProjectByProjectId(discoveredProjectId);
+				} catch (ProjectNotFoundException e) {
+					project = new Project(discoveredProjectId);
+					wall.getProjects().add(project);
+				}
 
-                updateProject(wall.getPluginHolder(), project);
-            }
-        }
-    }
+				updateProject(wall.getPluginHolder(), project);
+			}
+		}
+	}
 
-    public void updateWallProject(Wall wall, String projectName) throws ProjectNotFoundException {
-        Preconditions.checkNotNull(wall, "wall is a mandatory parameter");
-        Preconditions.checkNotNull(projectName, "projectName is a mandatory parameter");
+	public void updateWallProject(Wall wall, String projectName) throws ProjectNotFoundException {
+		Preconditions.checkNotNull(wall, "wall is a mandatory parameter");
+		Preconditions.checkNotNull(projectName, "projectName is a mandatory parameter");
 
-        Project project = wall.getProjectByName(projectName);
-        updateProject(wall.getPluginHolder(), project);
-    }
+		Project project = wall.getProjectByName(projectName);
+		updateProject(wall.getPluginHolder(), project);
+	}
 
-    void updateProject(PluginHolder pluginHolder, Project project) {
-        for(BuildConnectionPlugin service : pluginHolder.getBuildServices()) {
-            projectMergeService.merge(project, service);
-        }
-        for(QualityConnectionPlugin service : pluginHolder.getQualityServices()) {
-            projectMergeService.merge(project, service, metrics);
-        }
-        if (LOG.isDebugEnabled()) {
-            LOG.debug(project.toString());
-        }
-    }
+	void updateProject(PluginHolder pluginHolder, Project project) {
+		for (BuildConnectionPlugin service : pluginHolder.getBuildServices()) {
+			projectMergeService.merge(project, service);
+		}
+		for (QualityConnectionPlugin service : pluginHolder.getQualityServices()) {
+			projectMergeService.merge(project, service, metrics);
+		}
+		if (LOG.isDebugEnabled()) {
+			LOG.debug(project.toString());
+		}
+	}
 
-    /**
-     * @return null if no date could be estimated
-     * @throws ProjectNotFoundException
-     */
-    public Date getEstimatedFinishTime(Wall wall, String projectName) throws ProjectNotFoundException {
-        Preconditions.checkNotNull(wall, "wall is a mandatory parameter");
-        Preconditions.checkNotNull(projectName, "projectName is a mandatory parameter");
+	/**
+	 * @return null if no date could be estimated
+	 * @throws ProjectNotFoundException
+	 */
+	public Date getEstimatedFinishTime(Wall wall, String projectName) throws ProjectNotFoundException {
+		Preconditions.checkNotNull(wall, "wall is a mandatory parameter");
+		Preconditions.checkNotNull(projectName, "projectName is a mandatory parameter");
 
-        ProjectId projectId = wall.getProjectByName(projectName).getProjectId();
-        for(BuildConnectionPlugin service : wall.getPluginHolder().getBuildServices()) {
-            try {
-                Date estimatedFinishTime = service.getEstimatedFinishTime(projectId);
-                if (estimatedFinishTime != null) {
-                    return estimatedFinishTime;
-                }
-            } catch(ProjectNotFoundException e) {
-                if (LOG.isDebugEnabled()) {
-                    LOG.debug(e.getMessage());
-                }
-            }
-        }
-        return null;
-    }
+		ProjectId projectId = wall.getProjectByName(projectName).getProjectId();
+		for (BuildConnectionPlugin service : wall.getPluginHolder().getBuildServices()) {
+			try {
+				Date estimatedFinishTime = service.getEstimatedFinishTime(projectId);
+				if (estimatedFinishTime != null) {
+					return estimatedFinishTime;
+				}
+			} catch (ProjectNotFoundException e) {
+				if (LOG.isDebugEnabled()) {
+					LOG.debug(e.getMessage());
+				}
+			}
+		}
+		return null;
+	}
 
-    public int getLastBuildNumber(PluginHolder pluginHolder, ProjectId projectId) {
-        Preconditions.checkNotNull(pluginHolder, "pluginHolder is a mandatory parameter");
-        Preconditions.checkNotNull(projectId, "projectId is a mandatory parameter");
+	public int getLastBuildNumber(PluginHolder pluginHolder, ProjectId projectId) {
+		Preconditions.checkNotNull(pluginHolder, "pluginHolder is a mandatory parameter");
+		Preconditions.checkNotNull(projectId, "projectId is a mandatory parameter");
 
-        for (BuildConnectionPlugin service : pluginHolder.getBuildServices()) {
-            try {
-                return service.getLastBuildNumber(projectId);
-            } catch (ProjectNotFoundException e) {
-                if (LOG.isDebugEnabled()) {
-                    LOG.debug(e.getMessage());
-                }
-            } catch (BuildNotFoundException e) {
-                if (LOG.isDebugEnabled()) {
-                    LOG.debug(e.getMessage());
-                }
-            }
-        }
-        return PROJECT_NOT_BUILT_ID;
-    }
+		for (BuildConnectionPlugin service : pluginHolder.getBuildServices()) {
+			try {
+				return service.getLastBuildNumber(projectId);
+			} catch (ProjectNotFoundException e) {
+				if (LOG.isDebugEnabled()) {
+					LOG.debug(e.getMessage());
+				}
+			} catch (BuildNotFoundException e) {
+				if (LOG.isDebugEnabled()) {
+					LOG.debug(e.getMessage());
+				}
+			}
+		}
+		return PROJECT_NOT_BUILT_ID;
+	}
 
-    public State getState(PluginHolder pluginHolder, ProjectId projectId) {
-        Preconditions.checkNotNull(pluginHolder, "pluginHolder is a mandatory parameter");
-        Preconditions.checkNotNull(projectId, "projectId is a mandatory parameter");
+	public State getState(PluginHolder pluginHolder, ProjectId projectId) {
+		Preconditions.checkNotNull(pluginHolder, "pluginHolder is a mandatory parameter");
+		Preconditions.checkNotNull(projectId, "projectId is a mandatory parameter");
 
-        for (BuildConnectionPlugin service : pluginHolder.getBuildServices()) {
-            try {
-                return service.getState(projectId);
-            } catch (ProjectNotFoundException e) {
-                if (LOG.isDebugEnabled()) {
-                    LOG.debug(e.getMessage());
-                }
-            }
-        }
-        throw new RuntimeException("Project must have a state.");
-    }
+		for (BuildConnectionPlugin service : pluginHolder.getBuildServices()) {
+			try {
+				return service.getState(projectId);
+			} catch (ProjectNotFoundException e) {
+				if (LOG.isDebugEnabled()) {
+					LOG.debug(e.getMessage());
+				}
+			}
+		}
+		throw new RuntimeException("Project must have a state.");
+	}
 
-    public boolean isBuilding(PluginHolder pluginHolder, ProjectId projectId) {
-        Preconditions.checkNotNull(pluginHolder, "pluginHolder is a mandatory parameter");
-        Preconditions.checkNotNull(projectId, "projectId is a mandatory parameter");
+	public boolean isBuilding(PluginHolder pluginHolder, ProjectId projectId) {
+		Preconditions.checkNotNull(pluginHolder, "pluginHolder is a mandatory parameter");
+		Preconditions.checkNotNull(projectId, "projectId is a mandatory parameter");
 
-        for (BuildConnectionPlugin service : pluginHolder.getBuildServices()) {
-            try {
-                return service.isBuilding(projectId);
-            } catch (ProjectNotFoundException e) {
-                if (LOG.isDebugEnabled()) {
-                    LOG.debug(e.getMessage());
-                }
-            }
-        }
-        return false;
-    }
+		for (BuildConnectionPlugin service : pluginHolder.getBuildServices()) {
+			try {
+				return service.isBuilding(projectId);
+			} catch (ProjectNotFoundException e) {
+				if (LOG.isDebugEnabled()) {
+					LOG.debug(e.getMessage());
+				}
+			}
+		}
+		return false;
+	}
 
-    public Build findBuildByBuildNumber(Wall wall, String projectName, int buildNumber) throws BuildNotFoundException, ProjectNotFoundException {
-        Preconditions.checkNotNull(wall, "wall is a mandatory parameter");
-        Preconditions.checkNotNull(projectName, "projectName is a mandatory parameter");
+	public Build findBuildByBuildNumber(Wall wall, String projectName, int buildNumber) throws BuildNotFoundException,
+	        ProjectNotFoundException {
+		Preconditions.checkNotNull(wall, "wall is a mandatory parameter");
+		Preconditions.checkNotNull(projectName, "projectName is a mandatory parameter");
 
-        ProjectId projectId = wall.getProjectByName(projectName).getProjectId();
-        for (BuildConnectionPlugin service : wall.getPluginHolder().getBuildServices()) {
-            try {
-                Build build = service.findBuildByBuildNumber(projectId, buildNumber);
-                if (build != null) {
-                    return build;
-                }
-            } catch (BuildNotFoundException e) {
-                if (LOG.isDebugEnabled()) {
-                    LOG.debug(e.getMessage());
-                }
-            } catch (ProjectNotFoundException e) {
-                if (LOG.isDebugEnabled()) {
-                    LOG.debug(e.getMessage());
-                }
-            }
-        }
-        throw new BuildNotFoundException("No build #"+buildNumber+" for project "+projectId);
-    }
+		ProjectId projectId = wall.getProjectByName(projectName).getProjectId();
+		for (BuildConnectionPlugin service : wall.getPluginHolder().getBuildServices()) {
+			try {
+				Build build = service.findBuildByBuildNumber(projectId, buildNumber);
+				if (build != null) {
+					return build;
+				}
+			} catch (BuildNotFoundException e) {
+				if (LOG.isDebugEnabled()) {
+					LOG.debug(e.getMessage());
+				}
+			} catch (ProjectNotFoundException e) {
+				if (LOG.isDebugEnabled()) {
+					LOG.debug(e.getMessage());
+				}
+			}
+		}
+		throw new BuildNotFoundException("No build #" + buildNumber + " for project " + projectId);
+	}
 
 }
