3fe9e3aa41d4635f39f0a7de77f708b545a7474a
jsmadja <julien.smadja@gmail.com>
2011-05-28 15:24:30 +0200

    add unit tests

--- testProjects/it-coverage-project/librestry-core/src/test/java/fr/xebia/librestry/SuccessTest.java
+++ testProjects/it-coverage-project/librestry-core/src/test/java/fr/xebia/librestry/SuccessTest.java
@@ -1,12 +1,22 @@
 package fr.xebia.librestry;
+
+import junit.framework.Assert;
+
 import org.junit.Test;
+
+import fr.xebia.librestry.domain.Book;
 
 
 public class SuccessTest {
 
 	@Test
 	public void test_success1() {
+		Book book = new Book();
+		book.setAuthor("author");
+		book.setTitle("title");
 		
+		Assert.assertEquals("author", book.getAuthor());
+		Assert.assertEquals("title", book.getTitle());
 	}
 	
 	@Test
