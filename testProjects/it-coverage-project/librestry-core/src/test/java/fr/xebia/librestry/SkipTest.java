eed9682e5bf8f19e14c7ee650a17e5df5feff4f0
jsmadja <julien.smadja@gmail.com>
2011-05-28 14:24:06 +0200

    add skip tests

---
+++ testProjects/it-coverage-project/librestry-core/src/test/java/fr/xebia/librestry/SkipTest.java
@@ -0,0 +1,18 @@
+package fr.xebia.librestry;
+
+import org.junit.Ignore;
+
+public class SkipTest {
+
+	@Ignore
+	public void ignore_test1() {}
+	@Ignore
+	public void ignore_test2() {}
+	@Ignore
+	public void ignore_test3() {}
+	@Ignore
+	public void ignore_test4() {}
+	@Ignore
+	public void ignore_test5() {}
+
+}
---
+++ testProjects/it-coverage-project/librestry-it/src/it/java/fr/xebia/librestry/SkipIT.java
@@ -0,0 +1,20 @@
+package fr.xebia.librestry;
+
+import org.junit.Ignore;
+
+public class SkipIT {
+
+	@Ignore
+	public void ignore_test1() {}
+	@Ignore
+	public void ignore_test2() {}
+	@Ignore
+	public void ignore_test3() {}
+	@Ignore
+	public void ignore_test4() {}
+	@Ignore
+	public void ignore_test5() {}
+	@Ignore
+	public void ignore_test6() {}
+	
+}
