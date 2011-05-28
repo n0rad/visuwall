6ce082f53ff48cd7aa5bfc00657e72bb4c97d9b5
n0rad <dev@norad.fr>
2011-05-28 19:05:44 +0200

    - up tests

--- testProjects/successproject/src/test/java/net/awired/jwall/StatusTest.java
+++ testProjects/successproject/src/test/java/net/awired/jwall/StatusTest.java
@@ -8,6 +8,9 @@ public class StatusTest {
 	@Test
 	public void should_test() {
 		String value = System.getenv("status");
+		String valueProp = System.getProperty("status");
+		System.out.println("##########" + value);
+		System.out.println("##########" + valueProp);
 		if (value != null && value.trim().equals("")) {
 			System.out.println("status value :" + value);
 			if ("fail".equals(value)) {
