329a896487da5403940d207df0f6f2b6d3d966ff
jsmadja <julien.smadja@gmail.com>
2011-06-05 13:40:32 +0200

    fix #35 2 projets are not displayed with ci.awired.net

---
+++ component/it/src/it/java/net/awired/visuwall/core/service/ProjectServiceIT.java
@@ -0,0 +1,25 @@
+package net.awired.visuwall.core.service;
+
+import net.awired.visuwall.api.domain.ProjectId;
+import net.awired.visuwall.api.exception.BuildNotFoundException;
+import net.awired.visuwall.api.exception.ProjectNotFoundException;
+import net.awired.visuwall.api.plugin.BuildConnectionPlugin;
+import net.awired.visuwall.plugin.jenkins.JenkinsPlugin;
+
+import org.junit.Test;
+
+public class ProjectServiceIT {
+
+    @Test
+    public void github_issue_35() throws BuildNotFoundException, ProjectNotFoundException {
+        String jenkinsUrl = "http://ci.awired.net/jenkins/";
+        JenkinsPlugin jenkinsPlugin = new JenkinsPlugin();
+        BuildConnectionPlugin connection = (BuildConnectionPlugin) jenkinsPlugin.getConnection(jenkinsUrl, null);
+        int buildNumber = 29;
+        ProjectId projectId = new ProjectId("Acml");
+        projectId.setArtifactId("net.awired.aclm:aclm");
+        projectId.addId("JENKINS_ID", "Acml");
+        connection.findBuildByBuildNumber(projectId, buildNumber);
+    }
+
+}
