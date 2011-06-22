dc42aa6f4aa26d2b9f5ebc80cb26dfd984b57a01
jsmadja <julien.smadja@gmail.com>
2011-06-22 22:20:01 +0200

    add getMavenId method

---
+++ component/api/src/main/java/net/awired/visuwall/api/exception/MavenIdNotFoundException.java
@@ -0,0 +1,7 @@
+package net.awired.visuwall.api.exception;
+
+public class MavenIdNotFoundException extends Exception {
+
+    private static final long serialVersionUID = 1869112629963801100L;
+
+}
