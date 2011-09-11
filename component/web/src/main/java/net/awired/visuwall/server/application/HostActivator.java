13a9383fcb32db74ba5ffd78df8f70e56295897e
n0rad <dev@norad.fr>
2011-09-11 12:04:28 +0200

    - add OSGI container

---
+++ component/web/src/main/java/net/awired/visuwall/server/application/HostActivator.java
@@ -0,0 +1,31 @@
+package net.awired.visuwall.server.application;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class HostActivator implements BundleActivator {
+	private BundleContext context = null;
+
+	public HostActivator() {
+	}
+
+	public void start(BundleContext context) {
+		this.context = context;
+	}
+
+	public void stop(BundleContext context) {
+		this.context = null;
+	}
+
+	public Bundle[] getBundles() {
+		if (context != null) {
+			return context.getBundles();
+		}
+		return null;
+	}
+
+	public BundleContext getContext() {
+		return context;
+	}
+}
--- component/web/src/main/java/net/awired/visuwall/server/application/VisuwallContextLoaderListener.java
+++ component/web/src/main/java/net/awired/visuwall/server/application/VisuwallContextLoaderListener.java
@@ -16,22 +16,150 @@
 
 package net.awired.visuwall.server.application;
 
-import javax.servlet.ServletContextEvent;
-import net.awired.visuwall.core.application.common.ApplicationHelper;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
+import javax.servlet.ServletContextEvent;
+
+import net.awired.visuwall.api.plugin.VisuwallPlugin;
+import net.awired.visuwall.core.application.common.ApplicationHelper;
+import net.awired.visuwall.core.exception.NotFoundException;
+
+import org.apache.felix.framework.util.FelixConstants;
+import org.apache.felix.main.AutoProcessor;
 import org.fusesource.jansi.AnsiConsole;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.Constants;
+import org.osgi.framework.launch.Framework;
+import org.osgi.framework.launch.FrameworkFactory;
+import org.osgi.util.tracker.ServiceTracker;
 import org.springframework.web.context.ContextLoaderListener;
 
 public class VisuwallContextLoaderListener extends ContextLoaderListener {
 
-    @Override
-    public void contextInitialized(ServletContextEvent event) {
-        AnsiConsole.systemInstall();
-        String home = ApplicationHelper.findHomeDir();
-        System.setProperty(ApplicationHelper.HOME_KEY, home);
-        
-        ApplicationHelper.changeLogLvl();
-        super.contextInitialized(event);
-    }
+	private Framework osgi;
+	HostActivator m_activator;
+	private ServiceTracker m_tracker = null;
+
+	@Override
+	public void contextInitialized(ServletContextEvent event) {
+		AnsiConsole.systemInstall();
+		String home = ApplicationHelper.findHomeDir();
+		System.setProperty(ApplicationHelper.HOME_KEY, home);
+
+		ApplicationHelper.changeLogLvl();
+		super.contextInitialized(event);
+		launchOsgiContainer();
+	}
+
+	public void launchOsgiContainer() {
+		String bundleDir = System.getProperty(ApplicationHelper.HOME_KEY)
+				+ "/bundle";
+		String cacheDir = System.getProperty(ApplicationHelper.HOME_KEY)
+				+ "/cache";
+
+		Map<Object, Object> properties = new HashMap<Object, Object>();
+		properties.put(Constants.FRAMEWORK_STORAGE, cacheDir);
+		properties.put(AutoProcessor.AUTO_DEPLOY_DIR_PROPERY, bundleDir);
+		properties.put(AutoProcessor.AUTO_DEPLOY_ACTION_PROPERY,
+				"install,start");
+
+		m_activator = new HostActivator();
+		List<HostActivator> list = new ArrayList<HostActivator>();
+		list.add(m_activator);
+
+		properties.put(Constants.FRAMEWORK_SYSTEMPACKAGES_EXTRA,
+				"net.awired.visuwall.api.plugin.VisuwallPlugin; version=1.0.0");
+
+		properties.put(FelixConstants.SYSTEMBUNDLE_ACTIVATORS_PROP, list);
+
+		// (7) Add a shutdown hook to clean stop the framework.
+		String enableHook = "true";
+		if ((enableHook == null) || !enableHook.equalsIgnoreCase("false")) {
+			Runtime.getRuntime().addShutdownHook(
+					new Thread("Felix Shutdown Hook") {
+						public void run() {
+							try {
+								if (osgi != null) {
+									osgi.stop();
+									osgi.waitForStop(0);
+								}
+							} catch (Exception ex) {
+								System.err.println("Error stopping framework: "
+										+ ex);
+							}
+						}
+					});
+		}
+
+		try {
+			// (8) Create an instance and initialize the framework.
+			FrameworkFactory factory = getFrameworkFactory();
+			osgi = factory.newFramework(properties);
+			osgi.init();
+			// (9) Use the system bundle context to process the auto-deploy
+			// and auto-install/auto-start properties.
+			AutoProcessor.process(properties, osgi.getBundleContext());
+			// (10) Start the framework.
+			osgi.start();
+		} catch (Exception ex) {
+			System.err.println("Could not create framework: " + ex);
+			ex.printStackTrace();
+			System.exit(0);
+		}
+
+		m_tracker = new ServiceTracker(m_activator.getContext(),
+				VisuwallPlugin.class.getName(), null);
+		m_tracker.open();
+	}
+
+	public VisuwallPlugin<?> getPlugin(String name) throws NotFoundException {
+		Object[] services = m_tracker.getServices();
+		for (int i = 0; (services != null) && (i < services.length); i++) {
+			if (((VisuwallPlugin<?>) services[i]).getName().equals(name)) {
+				return (VisuwallPlugin<?>) services[i];
+			}
+		}
+		throw new NotFoundException("VisuwallPlugin service not found in osgi engine for name : " + name);
+	}
+
+	private FrameworkFactory getFrameworkFactory() throws Exception {
+		URL url = this
+				.getClass()
+				.getClassLoader()
+				.getResource(
+						"META-INF/services/org.osgi.framework.launch.FrameworkFactory");
+		if (url != null) {
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					url.openStream()));
+			try {
+				for (String s = br.readLine(); s != null; s = br.readLine()) {
+					s = s.trim();
+					// Try to load first non-empty, non-commented line.
+					if ((s.length() > 0) && (s.charAt(0) != '#')) {
+						return (FrameworkFactory) Class.forName(s)
+								.newInstance();
+					}
+				}
+			} finally {
+				if (br != null)
+					br.close();
+			}
+		}
+
+		throw new Exception("Could not find framework factory.");
+	}
+
+	public Bundle[] getInstalledBundles() {
+		// Use the system bundle activator to gain external
+		// access to the set of installed bundles.
+		return m_activator.getBundles();
+	}
 
 }
