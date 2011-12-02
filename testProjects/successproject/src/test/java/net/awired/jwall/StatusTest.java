36ce479921d0bc9cfe0aa568786234dcfd05ff79
n0rad <dev@norad.fr>
2011-12-02 10:02:21 +0100

    update test project to fail every % 2 minutes

--- testProjects/successproject/src/test/java/net/awired/jwall/StatusTest.java
+++ testProjects/successproject/src/test/java/net/awired/jwall/StatusTest.java
@@ -16,27 +16,31 @@
 
 package net.awired.jwall;
 
+import java.util.Calendar;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class StatusTest {
 
-	@Test
-	public void should_test() throws Exception {
-		String value = System.getenv("status");
-		String valueProp = System.getProperty("status");
-		System.out.println("##########" + value);
-		System.out.println("##########" + valueProp);
-		Thread.currentThread().sleep(60000L);
-		if (value != null && !value.trim().equals("")) {
-			System.out.println("status value :" + value);
-			if ("fail".equals(value)) {
-				Assert.fail("this is a fail project");
-			} else if ("error".equals(value)) {
-				throw new RuntimeException("This is a Error project");
-			}
-			
-		}
-	}
-	
+    @Test
+    public void should_test() throws Exception {
+        String value = System.getenv("status");
+        String valueProp = System.getProperty("status");
+        System.out.println("##########" + value);
+        System.out.println("##########" + valueProp);
+        Thread.currentThread().sleep(60000L);
+        //        if (value != null && !value.trim().equals("")) {
+        //            System.out.println("status value :" + value);
+        //            if ("fail".equals(value)) {
+        //                Assert.fail("this is a fail project");
+        //            } else if ("error".equals(value)) {
+        //                throw new RuntimeException("This is a Error project");
+        //            }
+        //
+        //        } 
+        int minutes = Calendar.getInstance().get(Calendar.MINUTE);
+        if (minutes % 2 == 0) {
+            Assert.fail("this is a failed project");
+        }
+    }
 }
