8181c1741db1009a9ed39918ce82aced6dc1bfbf
jsmadja <julien.smadja@gmail.com>
2012-02-11 16:43:48 +0100

    add demo plugin

---
+++ plugins/demo-plugin/src/main/java/net/awired/visuwall/plugin/demo/SoftwareProjectIds.java
@@ -0,0 +1,15 @@
+package net.awired.visuwall.plugin.demo;
+
+import net.awired.visuwall.api.domain.SoftwareProjectId;
+
+public class SoftwareProjectIds {
+
+    public static final SoftwareProjectId earth = new SoftwareProjectId("earth");
+    public static final SoftwareProjectId moon = new SoftwareProjectId("moon");
+    public static final SoftwareProjectId mars = new SoftwareProjectId("mars");
+    public static final SoftwareProjectId pluto = new SoftwareProjectId("pluto");
+    public static final SoftwareProjectId neptune = new SoftwareProjectId("neptune");
+    public static final SoftwareProjectId uranus = new SoftwareProjectId("uranus");
+    public static final SoftwareProjectId saturn = new SoftwareProjectId("saturn");
+
+}
